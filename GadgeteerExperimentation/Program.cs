ec90472c4416f5f7e9a5377a99666f3eaca6fd7c
Rob Scott <rob@deployerofcodes.com>
2014-10-25 22:25:27 -0400

    Put test code into a thread.

--- GadgeteerExperimentation/Program.cs
+++ GadgeteerExperimentation/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.SPOT;
+﻿using System.Threading;
+using Microsoft.SPOT;
 using System;
 using System.IO;
 
@@ -8,10 +9,33 @@ namespace GadgeteerExperimentation
     {
         private void ProgramStarted()
         {
+            var th = new Thread(Test);
+            th.Start();
+        }
+
+        private void Test()
+        {
             Debug.Print("Program started - memory = " + Debug.GC(false));
 
-            for (int preAlloc = 22000; preAlloc < 40000; preAlloc += 1000)
+            var isInserted = Mainboard.IsSDCardInserted;
+            var isMounted = Mainboard.IsSDCardMounted;
+            if(!isMounted)
+                Mainboard.MountStorageDevice(@"SD");
+
+            var mb = Mainboard;
+            var sd = mb.SDCardStorageDevice;
+            var vi = sd.Volume;
+            //vi.Format("FAT", 0, "TEST", true);
+            vi.Refresh();
+            var totalSize = vi.TotalSize;
+            var freeSpace = vi.TotalFreeSpace;
+
+            Debug.Print("totalSize = " + totalSize);
+            Debug.Print("freeSpace = " + freeSpace);
+
+            for (var preAlloc = 20000; preAlloc < 40000; preAlloc += 1000)
             {
+                Thread.Sleep(250);
                 Debug.GC(true);
                 RunTests(preAlloc);
             }
@@ -29,12 +53,14 @@ namespace GadgeteerExperimentation
             if (!Directory.Exists(directory))
                 Directory.CreateDirectory(directory);
 
-            const int length = 500; // rnd.Next(900) + 100;
+            const int length = 500;
             const int countFiles = 500;
             var sizes = new int[countFiles];
             var rnd = new Random();
             for (var idx = 0; idx < countFiles; idx++)
             {
+                Thread.Sleep(20);
+
                 sizes[idx] = length;
                 var data = new byte[length];
                 rnd.NextBytes(data);
@@ -43,16 +69,18 @@ namespace GadgeteerExperimentation
                 var path = Path.Combine(directory, fileName);
                 File.WriteAllBytes(path, data);
                 if (idx%25 == 0)
-                    Debug.Print("Writing " + idx + "; memory = " + Debug.GC(false));
+                    Debug.Print("Writing file " + idx + "; memory = " + Debug.GC(false));
             }
 
             for (var idx = 0; idx < countFiles; idx++)
             {
+                Thread.Sleep(20);
+
                 var fileName = "file-" + idx + ".txt";
                 var path = Path.Combine(directory, fileName);
                 var data = File.ReadAllBytes(path);
                 if (idx%25 == 0)
-                    Debug.Print("Reading " + idx + "; memory = " + Debug.GC(false));
+                    Debug.Print("Reading file " + idx + "; memory = " + Debug.GC(false));
                 if (data.Length != sizes[idx])
                     Debug.Print("*** LENGTH MISMATCH FOR FILE " + idx + "!!!");
             }
